165d351cab1f23cc10847e6c932336b5057ff047
Jakpok <jakpok97@gmail.com>
2014-07-09 13:55:55 +0200

    Initial commit

---
+++ JakPickUp/src/main/java/net/jakpok/jakpickup/JakPickup.java
@@ -0,0 +1,11 @@
+package net.jakpok.jakpickup;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class JakPickup extends JavaPlugin{
+
+	public JakPickup() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
